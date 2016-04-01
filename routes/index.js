e5361299d4d67e366ced628ff45ac8c25d120c54
Pierre Humberdroz <p.humberdroz@gmail.com>
2016-04-02 00:23:31 +0200

    small application changes. simple orders table.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+var orders = require("../models/orders");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
+router.get("/", function (req, res) {
+  orders.find({},function (err, allorders) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", {orders: allorders});
+    }
+  });
+});
 module.exports = router;
