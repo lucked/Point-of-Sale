086f4f678382af4fc30fa1b428dffd9ae3cbe16f
Pierre Humberdroz <p.humberdroz@gmail.com>
2016-04-06 14:10:10 +0200

    random customer generator added

--- seed.js
+++ seed.js
@@ -1,7 +1,9 @@
 var mongoose = require("mongoose");
 var product = require("./models/products");
+var customer = require("./models/customers");
 var Faker = require('faker');
 var arr = [];
+var customers = [];
 
 for (i=0; i<100; i++) {
     var obj= {
@@ -13,9 +15,20 @@ for (i=0; i<100; i++) {
     arr.push(obj);
 
 }
+for (i = 0; i < 100; i++) {
+    var obj = {
+        phone: Faker.phone.phoneNumber(),
+        name: Faker.name.lastName(),
+        mail: Faker.internet.email(),
+        street: Faker.address.streetName(),
+        housenumber: Math.floor((Math.random() * 200) + 1),
+        city: "Berlin",
+        zipcode: Faker.address.zipCode(),
+        story: Math.floor((Math.random() * 10) + 1)
+    };
+    customers.push(obj);
+}
 //console.log (arr);
-
-
 function seedDB(){
     //Remove all campgrounds
     product.remove({}, function(err){
@@ -33,7 +46,23 @@ function seedDB(){
             });
         });
     });
-}
 
+    customer.remove({}, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        console.log("removed products!");
+        customers.forEach(function (seed) {
+            customer.create(seed, function (err, campground) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log("added a customer");
+                }
+            });
+        });
+    });
+
+}
 
 module.exports = seedDB;
